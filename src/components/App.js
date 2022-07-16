374d4f1daf6be2210884bca090b997ae929de96d
wandera7 <julius.wandera@student.moringaschool.com>
2022-07-16 21:35:34 +0300

    Done.

--- src/components/App.js
+++ src/components/App.js
@@ -1,1 +1,27 @@
+import React, { useEffect, useState }  from "react"
+
 // create your App component here
+function App(){
+    const [image,setImage]=useState(null)
+
+    useEffect(()=>{
+        fetch('https://dog.ceo/api/breeds/image/random')
+       .then((res)=>res.json())
+       .then ((data)=>setImage(data.message))
+    },[])
+    if(!image){
+        return <p>Loading..</p>
+    }
+
+
+
+    return(
+        <div>
+        <img src={image} alt="A Random Dog"/>
+
+        </div>
+    )
+
+}
+
+export default App
